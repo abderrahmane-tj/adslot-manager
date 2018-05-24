5e633e61e784d084a367e6291d424d9dfaf3645e
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2018-05-24 15:08:42 +0000

    init new App structure

--- src/scripts/components/App/index.jsx
+++ src/scripts/components/App/index.jsx
@@ -1,7 +1,11 @@
 import React, {Component} from 'react';
 
-export default class App extends React {
+export default class App extends Component {
   render() {
-
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
   }
 }
