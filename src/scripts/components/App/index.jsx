680150d5d61548c9a401d6097ccd9b029b5fb8fe
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2018-05-26 15:37:12 +0000

    add spacing around header

--- src/scripts/components/App/index.jsx
+++ src/scripts/components/App/index.jsx
@@ -1,15 +1,16 @@
 import React, {Component} from 'react';
 import {Route, Switch, BrowserRouter} from 'react-router-dom';
-import {Grid, Header} from 'semantic-ui-react';
+import {Divider, Grid, Header} from 'semantic-ui-react';
 import AdForm from '../AdForm';
 import AdList from '../AdList';
 
 export default class App extends Component {
   render() {
     return (
-      <Grid container columns={1}>
+      <Grid container columns={1} style={{marginTop: '20px'}}>
         <Grid.Column>
           <Header as='h1'>Adslots Manager</Header>
+          <Divider hidden />
           <BrowserRouter>
             <Switch>
               <Route path="/ad/" component={AdForm}/>
