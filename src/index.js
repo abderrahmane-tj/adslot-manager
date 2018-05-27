ffdf6b2a7972847887ae45df7de10703cf151ca6
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2018-05-27 15:19:35 +0000

    add core-js package so we can polyfill String.includes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './scripts/components/App';
 
+// import vendor styles
 import 'semantic-ui-css/semantic.min.css';
+
+// import polyfills
+import 'core-js/fn/string/includes';
 
 ReactDOM.render(<App />, document.getElementById('root'));
